99b2e3f15723f70fd190e9e78cb30db6c4b390cb
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2024-09-20 15:01:22 +0200

    Add Favicon

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,6 +7,16 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:300,500,900" rel="stylesheet" />
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/favicon/site.webmanifest">
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#b40e44">
+        <link rel="shortcut icon" href="/favicon/favicon.ico">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
+
         <!-- Primary Meta Tags -->
         <title>Die Armee ist ein Trachtenverein!</title>
         <meta name="title" content="Die Armee ist ein Trachtenverein!" />
